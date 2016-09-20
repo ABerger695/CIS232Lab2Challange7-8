e87096e201d3785a5fea06cc92a8a71411b9701e
SK207_03 <SK207_03@K207_03.carrollcc.edu>
2016-09-19 20:50:37 -0400

    built customer class

--- PartnerLab/src/Customer.java
+++ PartnerLab/src/Customer.java
@@ -1,4 +1,28 @@
+public class Customer extends Person {
+	
+	String number;
+	boolean list;
+	
+	public Customer(String name, String address, String telephone, String number, boolean list){
+		super(name, address, telephone);
+		this.number = number;
+		this.list = list;
+	}
 
-public class Customer {
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public boolean isList() {
+		return list;
+	}
+
+	public void setList(boolean list) {
+		this.list = list;
+	}
 
 }
