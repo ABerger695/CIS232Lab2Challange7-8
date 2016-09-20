5d018ada7d4ad438ee6bf3fb0b389c073f20c73d
SK207_03 <SK207_03@K207_03.carrollcc.edu>
2016-09-19 20:41:02 -0400

    Built person class, started customer class.

--- PartnerLab/src/PartnerLab.java
+++ PartnerLab/src/PartnerLab.java
@@ -2,7 +2,10 @@
 public class PartnerLab {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person p = new Person("Alex", "123 Easy Street", "4431234567");
+		
+		System.out.printf("Person name: %s%nPerson address: %s%n"
+				+ "Person telephone number: %s%n",p.getName(), p.getAddress(), p.getTelephone());
 
 	}
 
---
+++ PartnerLab/src/Person.java
@@ -0,0 +1,36 @@
+
+public class Person {
+
+	public String name, address, telephone;
+	
+	public Person(String name, String address, String telephone){
+		this.name = name;
+		this.address = address;
+		this.telephone = telephone;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
+	public String getTelephone(){
+		return telephone;
+	}
+	
+}
