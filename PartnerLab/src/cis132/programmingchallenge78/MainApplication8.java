bafbd7837b1caaa4e60b31e33b850c0795abac72
SK207_04 <SK207_04@K207_04.carrollcc.edu>
2016-09-26 18:43:01 -0400

    Added do while loops

--- PartnerLab/src/cis132/programmingchallenge78/MainApplication8.java
+++ PartnerLab/src/cis132/programmingchallenge78/MainApplication8.java
@@ -11,22 +11,38 @@ public class MainApplication8 {
 		//This value is not needed so it is preset for ease of access
 		boolean list = true;
 		
+		String name = "";
+		String address = "";
+		String telephone = "";
+		String number = "";
+		double purchases = 0;
+		
 		//Giving customer variables a value
+		do {
 		System.out.println("Please enter customer name: ");
-		String name = keyboard.nextLine();
+		name = keyboard.nextLine();
+		} while (name.equals(""));
 		
+		do {
 		System.out.println("Please enter customer address: ");
-		String address = keyboard.nextLine();
+		address = keyboard.nextLine();
+		} while (address.equals(""));
 		
+		do {
 		System.out.println("Please enter customer phone number: ");
-		String telephone = keyboard.nextLine();
+		telephone = keyboard.nextLine();
+		} while (telephone.equals(""));
 		
+		do {
 		System.out.println("Please enter customer number: ");
-		String number = keyboard.nextLine();
+		number = keyboard.nextLine();
+		} while (number.equals(""));
 		
 		//Assigning the level of purchases
-		System.out.println("Please enter the amount of purchases the customer has made: ");
-		double purchases = keyboard.nextDouble();
+		do {
+		System.out.println("Please enter the amount of purchases the customer has made (More than zero): ");
+		purchases = keyboard.nextDouble();
+		} while (purchases == 0);
 		
 		//Creating the preferred customer class
 		PreferredCustomer customer = new PreferredCustomer(name, address, telephone, number, list, purchases);
